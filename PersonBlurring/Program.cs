9ae55bd479c399aa6f6e4e10b1950e0860fd7766
Mathew Miles <mathewm@javs.com>
2019-03-01 11:08:55 -0500

    Now tracks face better
    Known Potential Issues:
    - size of censor doesn't adjust as face grows / shrinks
    - having the face disappear and reappear causes censor shift

--- PersonBlurring/Program.cs
+++ PersonBlurring/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
+using System.Drawing;
 using OpenCvSharp;
+using DlibDotNet;
 
 namespace PersonBlurring
 {
@@ -25,22 +27,52 @@ namespace PersonBlurring
                 Thread.Sleep(500);
             }
             Console.WriteLine("Camera Opened");
+            CorrelationTracker tracker = new CorrelationTracker();
             Mat frame = new Mat();
             Mat gray = new Mat();
+            bool trackingFace = false;
             while (!cts.IsCancellationRequested)
             {
                 captureInstance.Read(frame);
                 if (frame.Empty())
                     break;
-                Cv2.CvtColor(frame, gray, ColorConversionCodes.BGRA2GRAY);
-                var faces = cascade.DetectMultiScale(       //detects faces and stores locations
-                    image: gray,
-                    scaleFactor: 1.1,
-                    minNeighbors: 3,
-                    flags: (HaarDetectionType)0,
-                    minSize: null
-                    );
-                foreach (Rect rect in faces)
+                if (!trackingFace)
+                {
+                    Cv2.CvtColor(frame, gray, ColorConversionCodes.BGRA2GRAY);
+                    var faces = cascade.DetectMultiScale(
+                        image: gray,
+                        scaleFactor: 1.1,
+                        minNeighbors: 3,
+                        flags: (HaarDetectionType)0,
+                        minSize: null
+                        );
+                    if (faces.Length > 0)
+                    {
+                        MatToBitmap(frame).Save(@"C:\Users\MatthewM\Documents\IdentityBlur\PersonBlurring\temp.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                        tracker.StartTrack(DlibDotNet.Dlib.LoadImage<int>(@"C:\Users\MatthewM\Documents\IdentityBlur\PersonBlurring\temp.bmp"), new DRectangle(faces[0].X - 10, faces[0].Y - 10, faces[0].X + faces[0].Width + 10, faces[0].Y + faces[0].Height + 20));
+                        trackingFace = true;
+                    }
+                }
+                else
+                {
+                    MatToBitmap(frame).Save(@"C:\Users\MatthewM\Documents\IdentityBlur\PersonBlurring\temp.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+                    double trackingQuality = tracker.Update(DlibDotNet.Dlib.LoadImage<int>(@"C:\Users\MatthewM\Documents\IdentityBlur\PersonBlurring\temp.bmp"));
+                    if (trackingQuality >= 5)
+                    {
+                        DRectangle position = tracker.GetPosition();
+                        Mat roi = new Mat(frame, new Rect((position.TopLeft.X < 0) ? 0 : (int)position.TopLeft.X,
+                                                          (position.TopLeft.Y < 0) ? 0 : (int)position.TopLeft.Y,
+                                                          (frame.Width < position.Width + position.TopLeft.X) ? frame.Width - (int)position.TopLeft.X : (int)position.Width,
+                                                          (frame.Height < position.Height + position.TopLeft.Y) ? frame.Height - (int)position.TopLeft.Y : (int)position.Height));
+                        Cv2.GaussianBlur(roi, roi, new OpenCvSharp.Size(101, 101), 0);
+                        Cv2.Rectangle(frame, new Rect((int)position.TopLeft.X, (int)position.TopLeft.Y, (int)position.Width, (int)position.Height), new Scalar(255, 255, 255));
+                    }
+                    else
+                    {
+                        trackingFace = false;
+                    }
+                }
+                /*foreach (Rect rect in faces)
                 {
                     Mat roi = new Mat(frame, new Rect((rect.X - rect.Width * .1 < 0) ? 0 : (int)(rect.X - rect.Width*.1),
                                                       (rect.Y - rect.Height * .2 < 0) ? 0 : (int)(rect.Y - rect.Height * .2),
@@ -48,9 +80,16 @@ namespace PersonBlurring
                                                       (frame.Height < rect.Height*1.2 + rect.Y) ? frame.Height-rect.Y + (int)(rect.Height * .2) : (int)(rect.Height * 1.4)));
                     Cv2.GaussianBlur(roi, roi, new Size(101, 101), 0);
                     Cv2.Rectangle(frame, rect, new Scalar(255, 255, 255), 1);
-                }
+                }*/
                 Cv2.ImShow("Feed", frame);
                 Cv2.WaitKey(1);
+            }
+        }
+        private static Bitmap MatToBitmap(Mat mat)
+        {
+            using (var ms = mat.ToMemoryStream())
+            {
+                return (Bitmap)Image.FromStream(ms);
             }
         }
     }
