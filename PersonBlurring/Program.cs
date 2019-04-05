19f12b16f55cc7f631df902d4df758a723821c7e
Mathew Miles <mathewm@javs.com>
2019-04-05 11:17:50 -0400

    Push to sync program

--- PersonBlurring/Program.cs
+++ PersonBlurring/Program.cs
@@ -72,15 +72,6 @@ namespace PersonBlurring
                         trackingFace = false;
                     }
                 }
-                /*foreach (Rect rect in faces)
-                {
-                    Mat roi = new Mat(frame, new Rect((rect.X - rect.Width * .1 < 0) ? 0 : (int)(rect.X - rect.Width*.1),
-                                                      (rect.Y - rect.Height * .2 < 0) ? 0 : (int)(rect.Y - rect.Height * .2),
-                                                      (frame.Width < rect.Width*1.1 + rect.X) ? frame.Width-rect.X + (int)(rect.Width*.1) : (int)(rect.Width*1.2),
-                                                      (frame.Height < rect.Height*1.2 + rect.Y) ? frame.Height-rect.Y + (int)(rect.Height * .2) : (int)(rect.Height * 1.4)));
-                    Cv2.GaussianBlur(roi, roi, new Size(101, 101), 0);
-                    Cv2.Rectangle(frame, rect, new Scalar(255, 255, 255), 1);
-                }*/
                 Cv2.ImShow("Feed", frame);
                 Cv2.WaitKey(1);
             }
